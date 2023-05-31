a6a4ac9cd739c2bc1ecddfa825fda7a94ddd8e5e
Andrei Karpenko <7498329@gmail.com>
2023-05-30 23:32:45 -0700

    add project setup

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require ('express');
-const PORT = process.env.PORT || 3001;
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const PORT = process.env.PORT || 4444;
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server starting on port ${PORT}`);
-})
-
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.json({
         message: "Hello from backend express.js",
     })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server starting on port ${PORT}`);
 })
